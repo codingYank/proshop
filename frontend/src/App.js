f84626208f1aeb8ab3cace6e034ee7471bb6654a
Tyler-Schmidt-Wissota <tyler.schmidt@wissotagroup.com>
2023-06-21 20:47:38 -0400

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import { Container } from "react-bootstrap"
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To Proshop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
