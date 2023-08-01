1cb75583a7cd1e3fb557f7e3df181c8429cd3c1c
Tyler-Schmidt-Wissota <tyler.schmidt@wissotagroup.com>
2023-08-01 13:51:43 -0400

    delete product

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -3,7 +3,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
-import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice'
+import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice'
 import { toast } from 'react-toastify'
 
 export const ProductListScreen = () => {
@@ -13,8 +13,18 @@ export const ProductListScreen = () => {
 
   const [createProduct, {isLoading: loadingCreate}] = useCreateProductMutation()
 
-  const deleteHandler = (id) => {
-    console.log('delete', id)
+  const [deleteProduct, { isLoading: loadingDelete}] = useDeleteProductMutation()
+
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure')) {
+      try {
+        await deleteProduct(id)
+        toast.success('Product Deleted')
+        refetch()
+      } catch (err) {
+        toast.error(err?.data?.message || err.error)
+      }
+    }
   }
 
   const createProductHandler = async () => {
@@ -42,6 +52,7 @@ export const ProductListScreen = () => {
       </Row>
 
       {loadingCreate && <Loader />}
+      {loadingDelete && <Loader />}
       { isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
         <>
           <Table striped hover responsive className='table-sm'>
