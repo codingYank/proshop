944f3c97531857f5e76c5cc16998a3a816c28769
codingYank <tyler.schmidt99@gmail.com>
2023-08-10 16:45:18 -0400

    fixed duplicate spinner

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -7,7 +7,7 @@ import { useGetTopProductsQuery } from '../slices/productsApiSlice'
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery()
 
-  return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>
+  return isLoading ? <></> : error ? <Message variant='danger'>{error}</Message>
   : (
     <Carousel pause='hover' className='bg-primary mb-4'>
       {products.map(product => (
